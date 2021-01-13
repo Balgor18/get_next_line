a7333f2d7bc6b6cf84f81c19ead23b67ec8d0dac
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-01-13 16:21:20 +0100

    first change

--- get_next_line.c
+++ get_next_line.c
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 16:16:15 by fcatinau          #+#    #+#             */
+/*   Updated: 2021/01/13 16:16:26 by fcatinau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-// fd = 0 1 2
-// 0 = stdin entree standart
-// 1 = stdout sortie standart
-// 2 = stderr erreur standart
-
+/*
+**  fd = 0 1 2
+**  0 = stdin entree standart
+**  1 = stdout sortie standart
+**  2 = stderr erreur standart
+*/
 
 int		get_next_line(int fd, char **line)
 {
@@ -14,5 +27,5 @@ int		get_next_line(int fd, char **line)
 
 	if (fd < 0 || line == NULL)
 		return (-1);
-	read(line,buf,0);
+	read(line, buf, 0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 16:13:23 by fcatinau          #+#    #+#             */
+/*   Updated: 2021/01/13 16:13:24 by fcatinau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <fcntl.h>
-#include <stdlib.h>
+# include <fcntl.h>
+# include <stdlib.h>
 
 int get_next_line(int fd, char **line);
 
