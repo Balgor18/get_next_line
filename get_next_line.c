86ee7db1f3c5da4993d303fe0c3ee53d7fa841b3
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-01-13 18:25:48 +0100

    debut comm

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 16:16:15 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/01/13 16:16:26 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/01/13 18:24:12 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 /*
-**  fd = 0 1 2
+**  fd(files descriptor) = 0 1 2
 **  0 = stdin entree standart
 **  1 = stdout sortie standart
 **  2 = stderr erreur standart
@@ -21,11 +21,41 @@
 
 int		get_next_line(int fd, char **line)
 {
-	char	*line_tmp;
-	size_t	a;
-	char	buf[BUFFER_SIZE];
+	char		*line_tmp; // temporaire donc pas besoin de gardez ses valeurs
+	size_t		a;
+	static char	buf[BUFFER_SIZE + 1];
+	static char	*lines = NULL;
+	int			end_buff;
 
-	if (fd < 0 || line == NULL)
+	/*
+	** lines est la variable qui sera afficher dans le terminal
+	** donc essentiel qu'elle soit static
+	** -------------------------------------------------
+	** Pourquoi j'aurai besoin que mon buffer soit static
+	** car a chaque tours il prendre le nombre de caracteres
+	** en plus et le buffer vas reasigner
+	** ----------------------------------
+	** verif que la buffer size est bonne
+	*/
+	if (fd < 0 || BUFFER_SIZE < 1 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
-	read(line, buf, 0);
+	/*
+	** pour read() le dernier 0 correspond a read only
+	**	return -1 pour erreur
+	*/
+	if (!(lines = ft_alloc(0)))
+		return (-1);
+/*
+** verifie si il peut alloc sans problem
+** -------------------------------------
+** cas ou nous sommes en fin de ligne
+*/
+	while (ft_strchr(lines, '\n') == NULL &&
+		(end_buff = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[end_buff] = '\0';
+		line_tmp = lines;
+		lines = ft_strjoin(line_tmp, buf);
+		free(line_tmp);
+	}
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,51 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 16:13:32 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/01/13 16:21:01 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/01/13 18:24:57 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char *str;
+
+	str = (char *)s;
+	while (*str != c)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		len1;
+	int		len2;
+	char	*str;
+
+	if (s1 && s2)
+	{
+		len1 = ft_strlen((char *)s1);
+		len2 = ft_strlen((char *)s2);
+		str = (char*)malloc(sizeof(char) * (len1 + len2 + 1));
+		if (str == NULL)
+			return (NULL);
+		i = -1;
+		while (s1[++i])
+			str[i] = s1[i];
+		i = -1;
+		while (s2[++i])
+		{
+			str[len1] = s2[i];
+			len1++;
+		}
+		str[len1] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
