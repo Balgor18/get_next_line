b19a7b40d59d2b3ecbd1587b61e4ac70fc4f9cae
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-01-18 15:02:25 +0100

    Start now

--- get_next_line.c
+++ get_next_line.c
@@ -5,80 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/11 09:38:44 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/01/11 11:41:52 by fcatinau         ###   ########.fr       */
+/*   Created: 2021/01/18 14:57:46 by fcatinau          #+#    #+#             */
+/*   Updated: 2021/01/18 15:01:07 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-static size_t	ft_bufflen(const char *s)
+int		get_next_line(int fd, char **line)
 {
-	int i;
 
-	i = 0;
-	while (s[i] != '\0' && s[i] != '\n')
-		i++;
-	return (i);
-}
-
-char			*ft_alloc(size_t size)
-{
-	char	*s;
-	char	*ptr;
-
-	s = (char *)malloc(sizeof(char) * (size + 1));
-	if (s == NULL)
-		return (NULL);
-	size = size + 1;
-	ptr = s;
-	while (size-- > 0)
-		*ptr++ = '\0';
-	return (s);
-}
-
-static char		*ft_save(char *lines, size_t *a)
-{
-	if (ft_strchr(lines, '\n'))
-	{
-		ft_strcpy(lines, ft_strchr(lines, '\n') + 1);
-		return (lines);
-	}
-	if (ft_bufflen(lines) > 0)
-	{
-		ft_strcpy(lines, ft_strchr(lines, '\0'));
-		*a = 0;
-		return (lines);
-	}
-	return (NULL);
-}
-
-int				get_next_line(int fd, char **line)
-{
-	static char		buf[BUFFER_SIZE + 1];
-	char			*line_tmp;
-	static char		*lines = NULL;
-	int				end_buff;
-	size_t			a;
-
-	a = 1;
-	if (fd < 0 || BUFFER_SIZE < 1 || line == NULL || read(fd, buf, 0) < 0)
-		return (-1);
-	if (lines == NULL && (lines = ft_alloc(0)) == NULL)
-		return (-1);
-	while (ft_strchr(lines, '\n') == NULL
-		&& (end_buff = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[end_buff] = '\0';
-		line_tmp = lines;
-		lines = ft_strjoin(line_tmp, buf);
-		free(line_tmp);
-	}
-	*line = ft_substr(lines, 0, ft_bufflen(lines));
-	if ((ft_save(lines, &a) != NULL) && a == 1)
-		return (1);
-	free(lines);
-	lines = NULL;
-	return (0);
 }
---
+++ main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/18 10:54:22 by fcatinau          #+#    #+#             */
+/*   Updated: 2021/01/18 14:58:17 by fcatinau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int		main(void)
+{
+	int		fd;
+	char	**line;
+
+	line = NULL;
+	if ((fd = open("MYFILE", O_RDONLY)) < 0)
+		return (-1);
+	if (get_next_line(fd, line) == -1)
+	{
+		printf("Crash !!!!!!!!!!!!");
+		return (0);
+	}
+	printf("1 : %s\n", *line);
+	free(*line);
+	get_next_line(fd, line);
+	printf("2 : %s\n", *line);
+	free(*line);
+	get_next_line(fd, line);
+	printf("3 : %s\n", *line);
+	free(*line);
+	get_next_line(fd, line);
+	printf("4 : %s\n", *line);
+	free(*line);
+	get_next_line(fd, line);
+	printf("5 : %s\n", *line);
+	free(*line);
+	close(fd);
+	return (0);
+}
