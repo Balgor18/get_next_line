39128de8a4e34a4b2f78e6e1c8ee288777a962dc
Balgor18 <catinaud.florian@gmail.com>
2021-01-18 20:07:36 +0100

    On avance

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,27 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 14:57:46 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/01/18 16:50:06 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/01/18 19:26:01 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		ft_ver_buf(char *buf)
+int		ft_ver_buf(char buf)
 {
-	size_t i;
-
-	i = 0;
-	while (buf[i])
-	{
-		if (buf[i] != '\n')
-			return (0);
-		i++;
-	}
-	return (1);
+	return ((buf != '\n') ? 1 : 0);
+/*
+** 	size_t i;
+** 
+** 	i = 0;
+** 	while (buf[i])
+** 	{
+** 		if (buf[i] == '\n')
+** 			return (0);
+** 		i++;
+** 	}
+** 	return (1);
+*/
 }
 
 int		ft_strlen(char *c)
@@ -36,33 +39,52 @@ int		ft_strlen(char *c)
 	return (i);
 }
 
-int		ft_malloc(char *c, int size)
+char	*ft_malloc( int size)
 {
-	if (!(malloc(sizeof(c) * size)))
-		return (0);
-	return (1);
+	char	*c;
+
+	if (!(c = malloc(sizeof(char *) * size)))
+		return (NULL);
+	return (c);
 }
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t		i;
-	printf("Valeur d'entre %s || %s || %zu \n", dest, src , size);
+	size_t		j;
+	//printf("Valeur d'entre %s || %s || %zu \n", dest, src , size);
 	i = 0;
+	j = 0;
+	while (dest[j])
+		j++;
 	if (size > 0)
 	{
-		printf("Je seg fqult ICI 2\n");
 		while (src[i] && i < (size - 1))
 		{
-			printf("Je seg fqult ICI 3\n");
-			dest[i] = src[i];
+			//printf("Valeur d'entre %c %d || %c %d\n", dest[i], dest[i], src[i], src[i]);
+			dest[j] = src[i];
 			i++;
+			j++;
 		}
 		dest[i] = '\0';
 	}
+	printf("dest = %s\n",dest);
 	while (src[i])
 		i++;
-	printf("Je seg fqult ICI 4\n");
 	return (i);
+}
+
+char	*ft_strcpy(char *dest, char src)
+{
+	int j;
+
+	j = 0;
+	while (dest[j])
+		j++;
+	dest[j] = src;
+	j++;
+	dest[j] = '\0';
+	return (dest);
 }
 
 int		get_next_line(int fd, char **line)
@@ -70,21 +92,30 @@ int		get_next_line(int fd, char **line)
 	char	buf[BUFFER_SIZE];
 	char	*line_tmp;
 	size_t	len;
+	unsigned int i;
 
+	i = 0;
 	line_tmp = NULL;
 	while ((len = read(fd, buf, BUFFER_SIZE)))
 	{
-		printf("Le seg fault des enfers\n");
-		if (ft_ver_buf(buf))
-			if (!ft_malloc(line_tmp, len))
+		while (ft_ver_buf(buf[i]) && buf[i])
+		{
+			printf("buf %c\n", buf[i]);
+			if (!(line_tmp = ft_malloc(len)))
 				return (-1);
-		printf("Le seg fault des enfers 2\n");
-		ft_strlcpy(line_tmp, buf, len);
-		printf("Le seg fault des enfers 3\n");
-		free(line_tmp);
+			//printf("line_temp = %s \n",line_tmp);
+			line_tmp = ft_strcpy(line_tmp, buf[i]);
+			//printf("line_temp = %s \n",line_tmp);
+			free(line_tmp);
+			i++;
+		}
+		if (ft_ver_buf(buf[i]))
+			break;
+		i = 0;
+		printf("line_tpm = %s\n",line_tmp);
 	}
-	printf("line_tmp %s \n", line_tmp);
-	ft_malloc(*line, ft_strlen(line_tmp));
+	//printf("line_tmp %s \n", line_tmp);
+	*line = ft_malloc(ft_strlen(line_tmp));
 	*line = line_tmp;
 	return (0);
 }
