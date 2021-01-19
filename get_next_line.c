da36068de5b42efc4c7974cd027f72211d653b45
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-01-19 16:54:42 +0100

    Petit a petit on avance

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 14:57:46 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/01/19 11:45:04 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/01/19 16:22:13 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,32 +60,36 @@ int			get_next_line(int fd, char **line)
 	char			*line_tmp;
 	size_t			len;
 	unsigned int	i;
-	unsigned int	j;
+	int				bre;
 
-	j = 0;
+	bre = 0;
 	i = 0;
 	line_tmp = NULL;
 	while ((len = read(fd, buf, BUFFER_SIZE)))
 	{
-		printf("%d && %c \n", ft_ver_buf(buf[i]), buf[i]);
-		while (ft_ver_buf(buf[i]) && buf[i])
+		while (i < len)// je boucle sur len pour voir toute les valeurs qu'il y a dans i
 		{
-			printf("buf %c\n", buf[i]);
-			//printf("line_tmp %s\n", line_tmp);
-			if (!(line_tmp = ft_malloc(len)))
-				return (-1);
-			line_tmp = ft_strcpy(line_tmp, buf[i]);
-			free(line_tmp);
+			if (ft_ver_buf(buf[i]))// je verifie que buf[i] ne soit pas = a \n
+			{
+				if (!(line_tmp = ft_malloc(len)))// change la size du malloc
+					return (-1);
+				line_tmp = ft_strcpy(line_tmp, buf[i]);
+				free(line_tmp);
+			}
+			else
+			{
+				bre = 1;
+				break ;
+			}
+			printf("line_tmp %s\n", line_tmp);
 			i++;
-			j++;
 		}
-		printf("Avant break || %s\n", line_tmp);
-		//printf("Je renvoie with - 1 %d || %c \n", ft_ver_buf(buf[i - 1]), buf[i - 1]);
-		//printf("Je renvoie %d || %c || %d \n", ft_ver_buf(buf[i]), buf[i], buf[i]);
-		if (!ft_ver_buf(buf[i - 1]) || j == 125)
+		if (bre)
 			break ;
 		i = 0;
 	}
+	printf("Le seg fqult \n");
+	printf("ft_strlen %p \n", ft_malloc(ft_strlen(line_tmp)));
 	*line = ft_malloc(ft_strlen(line_tmp));
 	*line = line_tmp;
 	return (0);
