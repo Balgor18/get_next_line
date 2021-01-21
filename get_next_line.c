31d7396b154cc00aed059fdd8525d075284ab0fb
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-01-21 13:30:42 +0100

    Go to nothing

--- get_next_line.c
+++ get_next_line.c
@@ -6,86 +6,12 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 14:57:46 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/01/21 13:27:54 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/01/21 13:30:19 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_get_save_line(char *save)
-{
-	char		*cpy;
-	size_t		i;
-	size_t		j;
-
-	i = 0;
-	j = 0;
-	if (!save)
-		return (0);
-	while (save[i] && save[i] != '\n')
-		i++;
-	if (!save[i])
-	{
-		free(save);
-		return (0);
-	}
-	if (!(cpy = malloc(sizeof(char) * ((ft_strlen(save) - i) + 1))))
-		return (0);
-	i++;
-	while (save[i])
-		cpy[j++] = save[i++];
-	cpy[j] = '\0';
-	free(save);
-	return (cpy);
-}
-
-char		*get_line(char *str)
-{
-	int		i;
-	char	*cpy;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (!(cpy = malloc(sizeof(char) * (i + 1))))
-		return (0);
-	i = 0;
-	while (str[i] && str[i] != '\n')
-	{
-		cpy[i] = str[i];
-		i++;
-	}
-	cpy[i] = '\0';
-	return (cpy);
-}
-
 int			get_next_line(int fd, char **line)
 {
-	char			*buffer;
-	static char		*save;
-	int				lire;
-
-	lire = 1;
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return (-1);
-	if (!(buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
-		return (-1);
-	while (!ft_is_end(save) && lire != 0)
-	{
-		if ((lire = read(fd, buffer, BUFFER_SIZE)) == -1)
-		{
-			free(buffer);
-			return (-1);
-		}
-		buffer[lire] = '\0';
-		save = join_str(save, buffer);
-	}
-	free(buffer);
-	*line = get_line(save);
-	save = ft_get_save_line(save);
-	if (lire == 0)
-		return (0);
-	return (1);
 }
