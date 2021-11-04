7530c375ff47dbd146ae966b11717e73232f8bbb
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-11-04 10:37:14 +0100

    Add kali file and testeur in the branch

--- get_next_line.h
+++ get_next_line.h
@@ -1,30 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/18 10:29:57 by rchallie          #+#    #+#             */
-/*   Updated: 2021/01/22 21:18:35 by fcatinau         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-
-int			get_next_line(int fd, char **line);
-void		*ft_memmove(void *dst, const void *src, size_t len);
-int			ft_is_end(char *str);
-char		*join_str(char const *char1, char const *char2);
-size_t		ft_strlen(const char *s);
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 2
+# ifndef BUFF_SIZE
+#  define BUFF_SIZE 2
 # endif
+
+// int	get_next_line(int fd, char **line, int reset);
+int	get_next_line(int fd, char **line);
 
 #endif
