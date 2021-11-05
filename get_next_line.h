57334e88c71a1d50cb33c8e82a54cbcf695a6805
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-11-05 17:48:50 +0100

    [WIP] Move fct in other file

--- get_next_line.h
+++ get_next_line.h
@@ -1,11 +1,48 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/errno.h>
+
 # ifndef BUFF_SIZE
 #  define BUFF_SIZE 2
 # endif
 
+typedef struct	s_gnl
+{
+	struct s_gnl	*next;
+	char			buf[BUFF_SIZE];
+}				t_gnl;
+
+struct	s_data
+{
+	size_t	line_size;
+	size_t	rd_size;
+	size_t	list_buff_size;
+	char	*ptrchr;
+
+	int		error;
+	t_gnl	*gnl;
+
+};
+
+struct 	s_mem
+{
+	char	str[BUFF_SIZE];
+	size_t	size;
+	int		status;
+};
+
 // int	get_next_line(int fd, char **line, int reset);
 int	get_next_line(int fd, char **line);
+
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *pointeur, int val, size_t size);
+void	ft_lstdelall(t_gnl **lst);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,1 +1,81 @@
 #include "get_next_line.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)s;
+	while (n--)
+	{
+		if (*p == (unsigned char)c)
+			return ((void *)p);
+		p++;
+	}
+	return (NULL);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dest && !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((char *)dest)[i] = ((const char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dst && !src)
+		return (NULL);
+	if (dst > src)
+	{
+		while (i < len)
+		{
+			((unsigned char *)dst)[len - 1] = ((unsigned char *)src)[len - 1];
+			len--;
+		}
+	}
+	else
+	{
+		while (i < len)
+		{
+			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	}
+	return (dst);
+}
+
+void	*ft_memset(void *pointeur, int val, size_t size)
+{
+	unsigned int	i;
+	unsigned char	*copy;
+
+	i = 0;
+	copy = (unsigned char *)pointeur;
+	while (i < size)
+		copy[i++] = (unsigned char)val;
+	pointeur = (void *)copy;
+	return (pointeur);
+}
+
+void	ft_lstdelall(t_gnl **lst)
+{
+	t_gnl	*tmp;
+
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		free(*lst);
+		(*lst) = tmp;
+	}
+}
